f8a99c5f70c96660b524db90e2912e3556260f85
huyue <huyue@sina.com>
2018-08-02 11:18:06 +0800

    完成初始化头和尾

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// 引入路由模块
+import VueRouter from 'vue-router';
+// 引入index组件
+import index from './components/index.vue';
+// 引入css样式
+import './assets/statics/site/css/style.css';
+// 引入路由中间件
+Vue.use(VueRouter)
+// 注册路由规则
+const router = new VueRouter({
+  routes :[
+    // 重定向
+    {
+      path:'/',
+      redirect:index
+    },
+    {
+      path:'/index',
+      component:index
+    }
+  ]
+})
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  // 渲染App组件
+  render:h =>h(App),
+  // 挂载到vue
+  router,
+}).$mount('#app');
